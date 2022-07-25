25f8a0258a442b062648577d4bb11e1f329a9e86
Your Name <gsamueljose@gmail.com>
2022-07-26 01:13:32 +0800

    added shell script

---
+++ build_godot.sh
@@ -0,0 +1,6 @@
+git clone --recursive -b 3.x https://github.com/godotengine/godot-cpp
+cd godot-cpp
+git submodule update --init --recursive
+scons platform=linux target=release
+cd ..
+scons platform=linux target=release
