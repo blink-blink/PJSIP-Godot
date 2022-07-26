93d5ca75b01d9001160e12748e551f806af2864e
Your Name <gsamueljose@gmail.com>
2022-07-26 09:48:33 +0800

    for debugging 3

---
+++ PJSIP-GodotLib/PJSIP_MyAccount.cpp
@@ -0,0 +1,37 @@
+#include "PJSIP_Common.h"
+#include "PJSIP_AudioStream.h"
+
+void MyAccount::removeCall(Call* call)
+{
+    for (std::vector<Call*>::iterator it = calls.begin();
+        it != calls.end(); ++it)
+    {
+        if (*it == call) {
+            calls.erase(it);
+            break;
+        }
+    }
+}
+
+void MyAccount::_onRegState(OnRegStateParam& prm)
+{
+    AccountInfo ai = getInfo();
+    std::cout << (ai.regIsActive ? "*** Register: code=" : "*** Unregister: code=")
+        << prm.code << std::endl;
+}
+
+void MyAccount::_onIncomingCall(OnIncomingCallParam& iprm)
+{
+    MyCall* call = new MyCall(*this, iprm.callId);
+    CallInfo ci = call->getInfo();
+    CallOpParam prm;
+
+    std::cout << "*** Incoming Call: " << ci.remoteUri << " ["
+        << ci.stateText << "]" << std::endl;
+
+    calls.push_back(call);
+    prm.statusCode = (pjsip_status_code)200;
+    call->answer(prm);
+    ASOwner->call_to_buffer_stream(call);   
+}
+
